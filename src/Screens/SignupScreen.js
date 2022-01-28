06248f1aa6185aca7ad523adc0b950a705b7e6ee
leonard33 <leonardkombo14@gmail.com>
2022-01-28 13:24:29 +0300

    sign up page completed

--- src/Screens/SignupScreen.js
+++ src/Screens/SignupScreen.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './SignupScreen.css'
 
 function SignupScreen() {
+
+  const signIn = (e) => {
+     e.preventDefault();
+  }
+  const register = (e) =>{
+    e.preventDefault();
+  }
+
   return <div className='signup_screen'>
    <form>
        <h1>SIGN UP</h1>
        <input type='email' placeholder='Email'/>
        <input type='password' placeholder='password' />
-       <button type='submit'>Sign In</button>
+       <button  onClick={signIn} type='submit'>Sign In</button>
        <h4><span className='signupscreen_gray'>New to netflix?</span>
         <span className='signupscreen_link'>Sign Up now.</span></h4>
    </form>
